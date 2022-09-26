c09ba757361ec0de19dc06e1b1101e7e543cd980
jieun0411 <annakim0411@naver.com>
2022-09-26 14:59:54 +0900

    콜라츠 추측

---
+++ LV1/cllatz_conjecture.js
@@ -0,0 +1,15 @@
+function solution(num) {
+  let answer = 0;
+  for (let i = 0; i < 500; i++) {
+    if (num === 1) return answer;
+
+    if (num % 2 === 0) {
+      num /= 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    answer++;
+  }
+
+  return (answer = -1);
+}
